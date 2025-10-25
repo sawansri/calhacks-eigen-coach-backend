7fd34cd348af03f3c5397799ed9178ccb3a338c8
Joe Wang <zezhou_wang@berkeley.edu>
2025-10-25 05:21:53 -0700

    feat: initializer

---
+++ agents/initializer.py
@@ -0,0 +1,5 @@
+# NOT AN AGENT!!!
+# Might not need to code since we can just hard code for hackathon.
+# Key idea is that this program intializes the question bank and non-sql memory
+
+print("Joe is so cool!")
